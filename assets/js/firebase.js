d656a3a63fa229f6291599896668bd4ed8cdbd37
James Martineau <James.Martineau@libertymutual.com>
2017-10-28 17:40:45 -0400

    chat messages working

---
+++ assets/js/firebase.js
@@ -0,0 +1,40 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBeUfmcYXZiY1LTrWHhpBrlGv8qlaetAFI",
+    authDomain: "rps-multiplayer-e6c3b.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-e6c3b.firebaseio.com",
+    projectId: "rps-multiplayer-e6c3b",
+    storageBucket: "",
+    messagingSenderId: "1082800212245"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+var messagesRef = database.ref("/messages");
+
+function postMessage() {
+
+    var messageText = $("#message").val();
+
+    database.ref("/messages").push({
+        playerName: "James",
+        messageText: messageText,
+        postTime: moment().format("MM-DD-YY, h:mm:ss a")
+    });
+
+}
+
+messagesRef.on("child_added", function (snapshot) {
+
+    let container = $("#posted");
+    let messageDiv = $("<div>");
+    let player = snapshot.val().playerName;
+    let message = snapshot.val().messageText;
+    let postTime = snapshot.val().postTime;
+
+    messageDiv.html("<b>"+player+"</b><small> " + postTime + "</small>: " + message);
+
+    container.append(messageDiv);
+    container.scrollTop(container.prop("scrollHeight"));
+    $("#message").val("");
+})
