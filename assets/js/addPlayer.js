1dcd597b213a7aa526898f1eb27ba0c0124c2aa9
James Martineau <James.Martineau@libertymutual.com>
2017-10-29 18:38:27 -0400

    some firebase functionality

--- assets/js/addPlayer.js
+++ assets/js/addPlayer.js
@@ -1,6 +1,7 @@
 var playersRef = database.ref("/players");
 var connectedRef = database.ref(".info/connected");
 var player
+var playerCount
 // --------------------------------------------------------------
 connectedRef.on("value", function (snap) {
 
@@ -19,11 +20,29 @@ function addPlayer() {
     //get user name input
     var playerName = $("#playerName").val();
 
+    // database.ref().child("players").orderByChild("playerNum").equalTo("1").once("value", snapshot => {
+    //     const userData = snapshot.val();
+    //     if (userData) {
+    //         console.log("Player 1 exists!");
+    //     } else {
+    //         console.log("Player 1 does NOT exist!");
+    //     }
+    // });
+
+    // database.ref().child("players").orderByChild("playerNum").equalTo("2").once("value", snapshot => {
+    //     const userData = snapshot.val();
+    //     if (userData) {
+    //         console.log("Player 2 exists!");
+    //     } else {
+    //         console.log("Player 2 does NOT exist!");
+    //     }
+    // });
+
     //add player to database
     playersRef.once("value", function (snapshot) {
 
         //look at current number of players on app
-        let playerCount = snapshot.numChildren();
+        playerCount = snapshot.numChildren();
         let playerIndex
         let existingPlayerNum
 
@@ -59,8 +78,58 @@ function addPlayer() {
             } else {
                 $(".player2-head").text(playerName);
             }
+
+            hideFormFields(); //
+        } else {
+            alert("Sorry, 2 people are already battling. You can join once one person leaves.")
         }
     });
 
     $("#playerName").val("");
 }
+
+playersRef.on("value", function (snapshot) {
+
+    //look at current number of players on app
+    playerCount = snapshot.numChildren();
+
+    if (playerCount >= 2) {
+        hideFormFields();
+    } else {
+        $(".input-group").css({
+            "display": "table",
+            "margin": "0 auto",
+            "width": "60%",
+            "padding": "0",
+            "float": "none"
+        });
+        $(".game-full").css("display", "none");
+    }
+
+});
+
+function hideFormFields() {
+    $(".input-group").css("display", "none");
+    $(".game-full").css({
+        "display": "block",
+        "color": "#fff",
+        "text-align": "center"
+    });
+}
+
+
+//listening for player changes and getting current player info
+database.ref("players").orderByChild("playerNum").on("value", function (snapshot) {
+
+    var query = firebase.database().ref("players").orderByKey();
+    query.once("value")
+        .then(function (snap) {
+            snap.forEach(function (childSnapshot) {
+                var playerKey = childSnapshot.key;
+                var playerData = childSnapshot.val();
+
+                // console.log(playerKey)
+                console.log(playerData.val().playerNum)
+            })
+        });
+});
